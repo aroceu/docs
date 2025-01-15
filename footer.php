fb74aa54020648a561ee71955183fe3f1f7863f3
Arrow Sparrow <dev@aroceu.cloud>
2025-01-15 15:15:45 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,13 @@
 
 <p align="center"><a href="#">to top</a> &bull; <a href="https://aroceu.com/atlas">sitemap</a> &bull; <a href="https://aroceu.com/docs">clear</a></p>
 
+<p style="text-align: center;">
+  This site is part of the <a href="https://meta-ring.hedy.dev/">Meta Ring</a>:<br/>
+  [<a href="https://meta-ring.hedy.dev/previous">← Previous</a>]
+  [<a href="https://meta-ring.hedy.dev/random">Random</a>]
+  [<a href="https://meta-ring.hedy.dev/next">Next →</a>]
+</p>
+
 
 </div>
 
