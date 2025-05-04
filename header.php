7cb720955714ebadcb8428a75d3475cb51c442f2
aroceu <dev@aroceu.cloud>
2025-05-04 01:31:06 -0400

    Update header.php

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 
         <meta name="description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house."/>
 
-<title>aroceu &bull; docs</title>
+<title>aroceu (docs)</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..700&family=Rochester&display=swap" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="https://aroceu.com/assets/fav.ico" />
