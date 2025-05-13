643a98c4e747873c86117c906be204092492ec39
aroceu <dev@aroceu.cloud>
2025-05-12 23:22:51 -0400

    Added header preview stuff

--- header.php
+++ header.php
@@ -2,12 +2,15 @@
 
 <head>
     
-    <meta name="viewport" content="initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noai, noimageai">
-
+    <meta charset="UTF-8">
         <meta name="description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house."/>
-
-<title>aroceu (docs)</title>
+        <meta property="og:title" content="aroceu (docs)" />
+        <meta property="og:description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house." />
+        <meta property="og:image" content="preview.png" />
+        <meta property="og:url" content="https://aroceu.com/docs" />
+       <title>aroceu (docs)</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..700&family=Rochester&display=swap" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="https://aroceu.com/assets/fav.ico" />
